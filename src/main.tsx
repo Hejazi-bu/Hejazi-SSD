589ca69645060acfeffbf2d9a656d79dec54d4d8
Mohamed Hegazi <hejazi.bu@gmail.com>
2025-08-16 20:17:13 +0400

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { UserProvider } from "./components/contexts/UserContext";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"; // ✅ بدل HashRouter
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <UserProvider>
       <BrowserRouter>
         <App />
-      </BrowserRouter>  
+      </BrowserRouter>
     </UserProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
